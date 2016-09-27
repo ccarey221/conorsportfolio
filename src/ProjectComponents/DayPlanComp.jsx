9cedad9cb1dd5bbd30f55fef60abb7607e14ca3f
Conor Carey <careyc@union.edu>
2016-09-26 18:52:30 -0700

    fix link to dayplan

--- src/ProjectComponents/DayPlanComp.jsx
+++ src/ProjectComponents/DayPlanComp.jsx
@@ -6,7 +6,7 @@ export default class DayPlanComp extends React.Component {
   render() {
     return (
         <div className="dayPlan">
-          <a target="_blank" href="http://www.trendwave.stream/">
+          <a target="_blank" href="http://www.dayplan.io/">
             <img className="dayPlanImage"src={dayPlanImage} role="presentation" />
           </a>
         </div>
